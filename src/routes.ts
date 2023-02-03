69c3b5f99c1e6d7001aa42fe87fe52e16a17264a
Toki <gorjan4ilijevski@gmail.com>
2023-02-03 17:33:53 +0100

    update user model and user route. create user service

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
-import { Express } from "express";
+import { Express, Request, Response } from "express";
 
 function routes(app: Express) {
-
+    app.get('/healthcheck', (req:Request, res: Response) => res.send(200))
 }
 
 export default routes
